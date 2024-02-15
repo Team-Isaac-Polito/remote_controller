b6c92aeec6ba215af901505162652e208448e9ff
Luca De Paola <60526200+luca-byte@users.noreply.github.com>
2024-02-15 01:52:27 +0100

    style: Adapt camera node to standard style

--- ros2_ws/src/remote_pi/remote_pi/camera.py
+++ ros2_ws/src/remote_pi/remote_pi/camera.py
@@ -9,6 +9,7 @@ class Camera(Node):
         super().__init__("camera")
         self.subscription_rgb = self.create_subscription(Image, "rgb_frame", self.rgb_frame_callback, 10)
         self.br_rgb = CvBridge()
+        self.get_logger().info("Camera node started")
 
     def rgb_frame_callback(self, data):
         current_frame = self.br_rgb.imgmsg_to_cv2(data)
@@ -17,11 +18,16 @@ class Camera(Node):
 
 
 def main(args = None):
-    rclpy.init(args = None)
-    camera = Camera()
-    rclpy.spin(camera)
-    camera.destroy_node()
-    rclpy.shutdown()
+    rclpy.init(args = args)
+    try:
+        camera = Camera()
+    except Exception as err:
+        print("Error while starting Camera node: " + str(err))
+        rclpy.shutdown()
+    else:    
+        rclpy.spin(camera)
+        camera.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
