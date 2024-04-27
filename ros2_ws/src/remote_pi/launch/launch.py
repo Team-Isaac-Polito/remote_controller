c84a4191bdbfdadc93dc44cf9a226d8bebcaa4a3
Luca De Paola <60526200+luca-byte@users.noreply.github.com>
2024-04-27 17:28:00 +0200

    feat: add RVIZ dependency

--- ros2_ws/src/remote_pi/launch/launch.py
+++ ros2_ws/src/remote_pi/launch/launch.py
@@ -1,11 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    share = get_package_share_directory("remote_pi")
+
     return LaunchDescription([
         Node(
             package='remote_pi',
             executable='remote',
             name='remote',
+        ),
+        Node(
+            package='rviz2',
+            name='rviz2',
+            executable='rviz2',
+            arguments=[f'-d {share}/config/rviz-config.rviz']
         )
     ])
