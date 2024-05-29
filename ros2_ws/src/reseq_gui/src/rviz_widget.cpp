f481a44e6ad0eb522e150f5346f9ab0f36e83e08
Luca De Paola <60526200+luca-byte@users.noreply.github.com>
2024-05-30 01:41:47 +0200

    feat: implement rviz init and rviz main functionalities
    Missing integrations with complete GUI

---
+++ ros2_ws/src/reseq_gui/src/rviz_widget.cpp
@@ -0,0 +1,72 @@
+#include "reseq_gui/rviz_widget.hpp"
+
+RVizWidget::RVizWidget(QApplication* app) : app_(app) {
+    using RosNode = rviz_common::ros_integration::RosNodeAbstraction;
+    // Create the main RViz widget
+    widget_ = new QWidget();
+    rviz_ros_node_ = std::make_shared<RosNode>("reseq_rviz_widget");
+
+    initializeRViz();
+    loadViews();
+    initDisplays();
+}
+
+void RVizWidget::pause() { manager_->stopUpdate(); }
+
+void RVizWidget::resume() { manager_->startUpdate(); }
+
+void RVizWidget::initializeRViz() {
+    app_->processEvents();
+    render_panel_ = new rviz_common::RenderPanel(widget_);
+    app_->processEvents();
+    render_panel_->getRenderWindow()->initialize();
+
+    auto clock = rviz_ros_node_.lock()->get_raw_node()->get_clock();
+    rviz_common::WindowManagerInterface* wm_ = nullptr;
+    manager_ = new rviz_common::VisualizationManager(
+        render_panel_, rviz_ros_node_, wm_, clock);
+
+    manager_->setFixedFrame("base_link");
+
+    view_manager_ = manager_->getViewManager();
+    render_panel_->initialize(manager_);
+    app_->processEvents();
+    manager_->initialize();
+}
+
+void RVizWidget::loadViews() {
+    rviz_common::ViewController* view;
+    view = view_manager_->create("rviz_default_plugins/Orbit");
+    view->setName("Third Person");
+    view->subProp("Distance")->setValue(3);
+    view->subProp("Pitch")->setValue(0.3);
+    view->subProp("Yaw")->setValue(3.1415);
+
+    view_manager_->add(view, 1);
+    view_manager_->setCurrentFrom(view);
+
+    view = view_manager_->create("rviz_default_plugins/TopDownOrtho");
+    view->setName("Top-Down");
+    view->subProp("Scale")->setValue(125);
+    view->subProp("Angle")->setValue(-1.57);
+
+    view_manager_->add(view, 2);
+}
+
+void RVizWidget::displayGrid(bool grid) { grid_->setEnabled(grid); }
+
+void RVizWidget::displayRobotModel(bool robot) {
+    robot_model_->setEnabled(robot);
+}
+
+void RVizWidget::initDisplays() {
+    grid_ = manager_->createDisplay("rviz_default_plugins/Grid", "grid", true);
+
+    robot_model_ = manager_->createDisplay(
+        "rviz_default_plugins/RobotModel", "RESEQ", true);
+    robot_model_->subProp("Description Topic")->setValue("/robot_description");
+}
+void RVizWidget::setView(RVizView view) {
+    int index = static_cast<int>(view);
+    view_manager_->setCurrentFrom(view_manager_->getViewAt(index));
+}
